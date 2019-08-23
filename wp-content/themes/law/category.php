36d6e2dd303e28c753a15fe43cb42f9e9f63eb0c
Alex-Smil <alexsmile39@gmail.com>
2019-08-23 08:51:37 +0300

    Урок 20 - Шаблоны index и single + added mysql-dump law.sql + перенос проект на ноутбук.

---
+++ wp-content/themes/law/category.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+
+<div id="fh5co-blog" class="fh5co-bg-section" style="background-color:#fcfcfc;">
+	<div class="container">
+		<div class="row">
+			<?php if ( have_posts() ) : while( have_posts() ) : the_post();  ?>
+				<div class="col-lg-4 col-md-4 my-col">
+					<div class="fh5co-blog animate-box">
+						<a href="<?php the_permalink(); ?>">
+							<?php
+							if (has_post_thumbnail()) { // если лого есть, то использ. его адрес
+								$img_url = get_the_post_thumbnail_url();
+							} else { // если лого нет, то картинка по умолч.
+								$img_url = get_template_directory_uri() . '/assets/images/moon.jpg';
+							}
+							?>
+
+							<img src="<?php echo $img_url; ?>" alt="<?php the_title(); ?>">
+						</a>
+						<div class="blog-text">
+							<span class="posted_on"><?php the_time('j M'); ?></span>
+							<span class="comment"><a href=""><?php echo get_comments_number(); ?><i class="icon-speech-bubble"></i></a></span>
+							<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+							<p><?php the_content(''); ?></p>
+							<a href="<?php the_permalink(); ?>" class="btn btn-primary my-btn"><?php _e('Read More', 'law'); ?></a>
+						</div>
+					</div>
+				</div>
+			<?php endwhile; ?>
+				<!-- post navigation -->
+			<?php else: ?>
+				<!-- no posts found -->
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
+
+<?php wp_footer(); ?>
--- wp-content/themes/law/single.php
+++ wp-content/themes/law/single.php
@@ -1,37 +1,29 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package Law
- */
+<?php get_header(); ?>
 
-get_header();
-?>
+<div id="fh5co-blog" class="fh5co-bg-section" style="background-color:#fcfcfc;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <?php if ( have_posts() ) : while( have_posts() ) : the_post();  ?>
+                        <?php
+                        if (has_post_thumbnail()) { // если лого есть, то использ. его адрес
+                            $img_url = get_the_post_thumbnail_url();
+                        } else { // если лого нет, то картинка по умолч.
+                            $img_url = get_template_directory_uri() . '/assets/images/moon.jpg';
+                        }
+                    ?>
+                    <h1><?php the_title(); ?></h1>
+                    <img class="pull-left low-post-image" src="<?php echo $img_url; ?>" alt="<?php the_title(); ?>">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main">
+                    <?php the_content(); ?>
+                <?php endwhile; ?>
+                    <!-- post navigation -->
+                <?php else: ?>
+                    <!-- no posts found -->
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</div>
 
-		<?php
-		while ( have_posts() ) :
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation();
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_sidebar();
-get_footer();
+<?php wp_footer(); ?>
